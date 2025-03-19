556912cc73c1d02000cb86c1d7bc4282d894d128
ivan2214 <bongiovanniivan12@gmail.com>
2025-03-19 13:41:00 -0300

    ci(husky): add commitlint to commit-msg hook
    This change ensures that commit messages follow the project's commit message guidelines by integrating commitlint into the commit-msg hook.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,6 +4,7 @@ import { RuleConfigSeverity } from "@commitlint/types";
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
   rules: {
+    "body-max-line-length": [RuleConfigSeverity.Warning, "always", 1000], // Aumenta la longitud máxima por línea
     "body-max-length": [RuleConfigSeverity.Warning, "always", 1000],
   },
 };
